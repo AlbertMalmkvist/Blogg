610c0010555dd119d188e97b95ba7c83da5e48aa
albert <malmkvist.albert@gmail.com>
2020-11-12 13:57:11 +0100

    k?

--- src/Resources/Static/components/BloggSummaryList.js
+++ src/Resources/Static/components/BloggSummaryList.js
@@ -1,16 +1,14 @@
-import auctionSummaryItem from './auctionSummaryItem.js'
+import bloggSummaryItem from './bloggSummaryItem.js'
 
 export default {
     components: {
-        auctionSummaryItem
+        bloggSummaryItem
     },
     data(){
      return {
          search:"",
      }
-
     },
-   
    template: ` 
    <div>
    <div class="search-wrapper">
@@ -18,40 +16,18 @@ export default {
     <input type="text" v-model="search" placeholder="Search title.."/>
     </div>
     <ul>
-        <auctionSummaryItem v-for="auction of auctions" :key="auction.id" :auction="auction"
-        v-if="Date.now() >= Date.parse(auction.start_time) && Date.now() < Date.parse(auction.end_time)" />
+        <bloggSummaryItem v-for="blogg of bloggs" :key="blogg.id" :blogg="blogg"
+        v-if="Date.now() >= Date.parse(blogg.start_time) && Date.now() < Date.parse(blogg.end_time)" />
      </ul>
      </div>
     `,
-   async created() {
-            //console.log('Adding setInterval...')
-            this.fetchInterval = setInterval(async () => {
-                let auction_ids = this.$store.state.auctions.map(auction => auction.id.toString())
-                let auctions_query_string = auction_ids.join(',')
-                let bids = await fetch('/rest/bids/highest?auctions=' + auctions_query_string)
-                bids = await bids.json()
-                //console.log('bids...', bids)
-            	this.$store.commit('updateHighestBids', bids)
-            }, 1000)
-    },
-    beforeDestroy() {
-        //console.log('Removing setInterval...')
-        clearInterval(this.fetchInterval)
-    },
     computed: {
-        auctions() {
-            return this.$store.state.auctions.filter((auction) => {
-                return auction.title.toLowerCase().match(this.search.toLowerCase());
+        bloggs() {
+            return this.$store.state.bloggs.filter((blogg) => {
+                return blogg.title.toLowerCase().match(this.search.toLowerCase());
             })
         }
-
-        
     },
     methods: {
-
     }
 }
-
-
-// bids must be joined in to be able to see current bid. 
-// Later feature
--- src/Resources/Static/components/bloggSummaryItem.js
+++ src/Resources/Static/components/bloggSummaryItem.js
@@ -1,10 +1,4 @@
-import addNewBid from './addNewBid.js'
-
 export default {
-    components:{
-        addNewBid
-
-    },
     props: [
         'auction'
     ],
@@ -13,15 +7,8 @@ export default {
         <li
             @click.self="showAuctionDetails(auction.id)"
             class="auction-card">
-                <div @click.self="showAuctionDetails(auction.id)" class="auction-card-photo" :style="{'background-image': 'url(' + auction.main_image + ')'}">
-                </div>
-                    Title: {{ auction.title }} <br>
-                    Ending: {{ auction.end_time }} <br>
-                    Highest bid: {{ auction.highestBid }} <br>
-                    Seller: {{ auction.sellerUsername }} <br/>
-                   
-            <addNewBid :auction="auction" v-if="Date.parse(this.auction.end_time) > Date.now() && this.$store.state.user && this.$store.state.user.id !== auction.seller"/>
-            <span v-else-if= "Date.parse(this.auction.end_time) < Date.now()">***Auction expired***</span>
+                    Title: {{ auction.title }} <br>  
+                    <span v-else-if= "Date.parse(this.auction.end_time) < Date.now()">***Auction expired***</span>
         </li>
         </div>
     `,
