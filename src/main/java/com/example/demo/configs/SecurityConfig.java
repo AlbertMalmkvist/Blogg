ae9ce00dc79d29adbf7ee691be2f14164272379e
albert <malmkvist.albert@gmail.com>
2020-12-04 13:35:45 +0100

    I got it working!!

--- src/main/java/com/example/demo/configs/SecurityConfig.java
+++ src/main/java/com/example/demo/configs/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.configs;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,9 +14,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
 
             .csrf().disable()
-            .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/rest/**").authenticated()
-            .and()
             .formLogin()
             .loginPage("/login")
         ;
